aaecf0bd607ef7790a96c3d9918b2806d5ec1570
Keith Schulze <keith.schulze@gmail.com>
2019-04-11 07:10:05 +1000

    Update deploy machinery

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,18 +10,19 @@ StackName="keithschulze-stack"
 
 aws sts get-caller-identity >/dev/null 2>&1 || { echo "Can't find AWS credentials" >&2; exit 1; }
 
-cloudformation_deploy() {
-    local STDERR=$(( aws cloudformation deploy "$@" ) 2>&1)
-    local EXIT_CODE=$?
-    echo ${STDERR} 1>&2
-    if [[ "${EXIT_CODE}" -eq "255" && ("${STDERR}" =~ "No changes to deploy" || "${STDERR}" =~ "No updates are to be performed" || "${STDERR}" =~ "The submitted information didn't contain changes") ]]; then
-        return 0;
-    fi
-    return ${EXIT_CODE}
+deploy_cloudformation() {
+  local templateFile=$1
+  local stackName=$2
+  local params="${@:3}"
+
+  aws cloudformation deploy \
+      --template-file "$templateFile" \
+      --stack-name "$stackName" \
+      --no-fail-on-empty-changeset \
+      --parameter-overrides $params
 }
 
-cloudformation_deploy --template-file deploy/s3-website.yaml --stack-name $StackName --parameter-override RootDomainName=$DomainName
+deploy_cloudformation "deploy/s3-website.yaml" "$StackName" "RootDomainName=$DomainName"
 
-# BucketURL=$(aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[0].Outputs[?OutputKey==`BucketURL`].OutputValue' --output text)
-#
-# aws s3 sync _site s3://$BucketURL/
+BucketName=$(aws cloudformation describe-stacks --stack-name "$StackName" --query 'Stacks[0].Outputs[?OutputKey==`BucketURL`].OutputValue' --output text)
+aws s3 sync _site "s3://$BucketName/"
